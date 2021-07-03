58a9d5acf94d468e40db2c1692ce378ff9ec649d
Kalindu Wasuka <38309732+kalinduwasuka@users.noreply.github.com>
2021-07-04 04:27:54 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$token= '<Enter your facebook access token';
+
+//https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials
+?>
